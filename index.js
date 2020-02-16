5487d825295f8abfb630a1cd6627944deae63bf0
goconjazz <2018116838@ust-ics.mygbiz.com>
2020-02-16 23:40:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function validate(){
+
+    var eventtype = document.getElementById("TYPE").value;
+    var eventname = document.getElementById("TITLE").value;
+    var eventwhen = document.getElementById("WHEN").value;
+    var eventuntilwhen = document.getElementById("UNTILWHEN").value;
+    var eventpost = document.getElementById("POST").value;
+    
+} 
+
